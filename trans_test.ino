d7ea2d57c75066a2981756e0970d3e056e775476
walkerhensley <89801430+walkerhensley@users.noreply.github.com>
2021-09-22 18:43:50 -0400

    Update trans_test.ino

--- trans_test.ino
+++ trans_test.ino
@@ -2,7 +2,9 @@
 #include <SPI.h> // Not actually used but needed to compile
 
 RH_ASK driver;
-int testSend = 126;
+int testSend = 512;
+int testSendTwo = 10;
+int testArray[] = {512,10};
 void setup()
 {
     Serial.begin(9600);    // Debugging only
@@ -16,9 +18,10 @@ void loop()
     
     //int x = 12345;
     //driver.send((uint8_t *)&x, 2);
-    
-    driver.send((uint8_t *)&testSend, (uint8_t)2);
+    driver.send((uint8_t *) &testSendTwo,(uint8_t)2);
+    driver.send((uint8_t *)&testSend, (uint8_t)2); //sending testSend value over
+    driver.send((uint8_t *)&testArray[0], (uint8_t)2);
+    driver.send((uint8_t *)&testArray[1], (uint8_t)2);
     driver.waitPacketSent();
     delay(1000);
 }
-
