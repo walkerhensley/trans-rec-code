bf9cef64a4d1f145663e517e0565ad4dcdba26a2
walkerhensley <89801430+walkerhensley@users.noreply.github.com>
2021-09-23 20:59:50 -0400

    Update rec_test.ino

--- rec_test.ino
+++ rec_test.ino
@@ -1,12 +1,36 @@
 #include <RH_ASK.h>
 #include <SPI.h> // Not actualy used but needed to compile
+#include <Stepper.h>
+#include <PWMServo.h>
+
 
 RH_ASK driver;
 
-int val =0; 
+int val =0;
 int valTwo = 0;
 uint8_t valArray[2];
 uint8_t nBytesReceived = 2;
+int servoValue;
+int stepperValue;
+int turn;
+int lastStepper;
+
+static const int STEPS_PER_REVOLUTION     = 32 * 64;
+static const int PIN_IN1_BLUE             = 7;
+static const int PIN_IN2_PINK             = 8;
+static const int PIN_IN3_YELLOW           = 9;
+static const int PIN_IN4_ORANGE           = 10;
+static const int PIN_BUTTON               = 4;
+
+Stepper myStepper(STEPS_PER_REVOLUTION, PIN_IN1_BLUE, PIN_IN3_YELLOW, PIN_IN2_PINK, PIN_IN4_ORANGE);
+PWMServo myServo;
+
+
+struct yo {
+  int testSend;
+  int testSendTwo;
+ 
+};
 void setup()
 {
     Serial.begin(9600);  // Debugging only
@@ -16,21 +40,64 @@ void setup()
 
 void loop()
 {
-    uint8_t buf[2];
+ 
+    uint8_t buf[sizeof(yo)];
 
-    uint8_t buflen = sizeof(buf);
-    if  (driver.recv((uint8_t *) (&valArray[0]), 2)) // Non-blocking //storing value of sent value in val
+    uint8_t buflen = sizeof(yo);
+  
+    if  (driver.recv(buf, &buflen)) // Non-blocking //storing value of sent value in val
     {
       int i;
       // Message with a good checksum received, dump it.
+
+      yo test;
+
+      memcpy(&test, &buf, sizeof(buf));
       Serial.print("Message: ");
-      Serial.println(valArray[0]);         
+      Serial.println(test.testSend);   
+      stepperValue = test.testSend;
+      delay(500);
+     Serial.println(test.testSendTwo);           
+    servoValue = test.testSendTwo;
+//int lastTest = stepperValue;
+myStepper.step(10);
+myStepper.setSpeed(stepperValue);
+//myStepper.setSpeed(7);
+//myStepper.step(stepperValue);
+//myStepper.setSpeed(stepperValue);
+//adding values to motor
+/*
+if (stepperValue > 0)
+{
+myStepper.setSpeed(stepperValue);
+//myStepper.setSpeed((int)(abs(stepperValue -512) / 30));
+myStepper.step(10); 
+//turning code
+
+}
+
+if (stepperValue < 470)
+{
+  myStepper.setSpeed((int)(abs(stepperValue-512)/30));
+    myStepper.step(-10);
+//turning code
+
+
+  turn = servoValue;
+turn = map(turn,0,1023,0,180);
+myServo.write(turn);
+
+  }
+*/
+//lastStepper = stepperValue;
     }
-    delay(500);
-    if (driver.recv((uint8_t *) (&valArray[1]), 2))
-    {
-      Serial.print("Message Two: ");
-      Serial.println(valArray[1]);
-      }
+//    delay(500);
+//    if (driver.recv(buf[1], &buflen))
+//    {
+//      Serial.print("Message Two: ");
+//      Serial.println(buf[1]);
+//      }
+ //myStepper.setSpeed(lastStepper);
+   //myStepper.step(10);
 }
 
